58c8999fef39c4dc6c7f3a15ca961f4499ca463b
Wayne Yang <wyang@Waynes-MacBook-Pro.local>
2014-02-21 20:57:18 -0800

    add js helper functions that take API responses and construct chart data

--- script/habari_charts.js
+++ script/habari_charts.js
@@ -1,3 +1,87 @@
+var NikePlus = {
+    /**
+     * jsonRespStr - response of list activities /me/sport/activities
+     */
+    createDistanceChartData: function(jsonRespStr) {
+        var obj = JSON.parse(jsonRespStr);
+        obj = obj.data;
+        var result = [];
+        for (var i = 0; i < obj.length; i++) {
+            result.push({
+                x: new Date(obj[i].startTime),
+                y: obj[i].distance,
+                calories: obj[i].calories,
+            });
+        }
+        return result;
+    },
+    /**
+     * jsonRespStr - response of list activities /me/sport/activities
+     */
+    createCaloriesChartData: function(jsonRespStr) {
+        var obj = JSON.parse(jsonRespStr);
+        obj = obj.data;
+        var result = [];
+        for (var i = 0; i < obj.length; i++) {
+            result.push({
+                x: new Date(obj[i].startTime),
+                y: obj[i].calories,
+                distance: obj[i].distance,
+            });
+        }
+        return result;
+    },
+};
+
+
+var Strava = {
+    /**
+     * construct a data series from response of /api/v3/athlete/activities
+     *
+     * jsonRespStr - response string of the activities list /api/v3/athlete/activities
+     * activityType - optional.  ie. ride, run, swim, etc.
+     */
+    createDistanceChartData: function(jsonRespStr, activityType) {
+        var obj = JSON.parse(jsonRespStr);
+        var result = [];
+        for (var i = 0; i < obj.length; i++) {
+            // filter by activity type
+            if (typeof activityType == 'undefined' 
+                || (typeof activityType == 'string' 
+                && activityType.toUpperCase() == obj[i].type.toUpperCase())) {
+                result.push({
+                    x: new Date(obj[i].start_date),
+                    y: obj[i].distance,
+                    calories: obj[i].calories,
+                });
+            }
+        }
+        return result;
+    },
+
+    /**
+     * jsonRespStr - response string of the activities list /api/v3/athlete/activities
+     */
+    createCaloriesChartData: function(jsonRespStr, activityType) {
+        var obj = JSON.parse(jsonRespStr);
+        var result = [];
+        for (var i = 0; i < obj.length; i++) {
+            // filter by activity type
+            if (typeof activityType == 'undefined' 
+                || (typeof activityType == 'string' 
+                && activityType.toUpperCase() == obj[i].type.toUpperCase())) {
+                result.push({
+                    x: new Date(obj[i].start_date),
+                    y: obj[i].calories,
+                    distance: obj[i].distance,
+                });
+            }
+        }
+        return result;
+    },
+};
+
+
 $(function () {
     $('#distance_container').highcharts({
         chart: {
@@ -26,7 +110,6 @@ $(function () {
                 s = Highcharts.dateFormat('%m/%d/%Y', this.x) + '<br />';
                 s += 'Distance: ' + this.y + ' miles' + '<br />';
                 s += 'Calories: ' + this.point.calories + '<br />';
-                console.log(this);
                 return s;
             }
         },
@@ -130,7 +213,5 @@ $(function () {
         },
         ]
     });
-
-
 });
 
